c377e3ac009ab50cc83697b5b1321346fbd4f8b1
lasermtv07 <laserman.minecraft@seznam.cz>
2024-03-23 15:58:27 +0100

    *cries* did something i guess

--- index.php
+++ index.php
@@ -6,29 +6,17 @@
 body {
 	margin:0;
 	padding:0;
+	min-height:100%;
 }
 main {
 	width:80%;
 	margin: 0 10%;
-	position; absolute;
-	left:50%;
-	padding-top: 10px;
-	display: flex;
-}
-.left {
-	padding: 15px;
-	margin: 10px;
-	background-color: #8cccff;
-	max-width: 306px;
-	border: 2px dotted #2984d6;
-}
-.right {
 	padding: 10px;
+	background-color: white;
+	position: absolute;
+	height: 100%;
 }
-.right h2 {
-	padding-top:0;
-	margin-top:0;
-}
+
 li {
 	list-style-type:"> ";
 }
@@ -39,46 +27,20 @@ a {
 a:hover {
 	text-decoration-line: underline;
 }
+img {
+	float:left;
+	padding: 5px;
+}
 	</style>
 </head>
 <body>
+<?php
+echo file_get_contents("menu.html");
+?>
 	<main>
-	<div class=left>
-		<img src="misa.png" alt="me :D" />
-		<br />
-		<b><center>lasermtv07 aka misa</center></b>
-		<hr />
-		My favourite number is <i>i</i> and you cannot do anything about it.
-		<hr />
-		<b>socials</b>
-		<hr />
-		<ul>
-			<li><a href="https://github.com/lasermtv07">github</a></li>
-			<li><a href="https://lasermtv.itch.io/">itch.io</li>
-			<li><a href="https://spacehey.com/profile?id=2559698">spacehey</a></li>
-			<li><a href="https://mastodon.social/@misa2007">mastodon</a></li>
-		</ul>
-		<hr />
-			<b>personal info</b>
-		<hr />
-		<ul>
-			<li>name: Michal Chmelar</li>
-			<li>age: 17</li>
-			<li>country: Czechia</li>
-			<li>studium: An IT professional school</li>
-		</ul>
-		<hr />
-		<b>interests</b>
-		<hr />
-		<ul>
-			<li>programming</li>
-			<li>electronics</li>
-			<li>math</li>
-			<li>esolangs</li>
-		</ul>
-	</div>
-	<div class=right>
+
 		<h2>Hi,</h2>
+	<img src=misa.png width=145 />
 		my name is <b>lasermtv07</b>, in real life named <b>Michal Chmelar</b>, and I am a 17 y.o. Czech programming student and computer enthusiast. I am studying database system. We still learn basic PHP (which I love), text files and SQL, which is not the <i>most modern</i> stuff, but to some extent I like that since it just works.
 Unlike having to beat my head against a wall to know what in the hell is <i>hydration</i>. Other than that, I also try to learn other technologies relating to low level development. I am also learning digital electronics and even try to dabble into analog stuff, like oscillators or amplifiers.
 I also have a weird obsession with <a href="https://esolangs.org/wiki/Esoteric_programming_language">esoteric</a> programming languages. So far Ive written a few esolang and made a few more <i>complicated</i> programs in brainfuck. </br></br>
@@ -87,9 +49,5 @@ I use <b>xterm</b>, because, as Ive stated previously, I am crazy.
 Sadly, I am not yet crazy enough to use a standalone WM (I mean I tried, havent stuck with it), so I use <b>LXDE</b>. I explore da interwebs using the <b>Firefox</b> browser. 
 For circuit simulations I use <b>Logisim</b>. I could talk about some other stuff I suppose, but no one cares about that anyway.
 <br />
-<h2>Github</b>
-<i>TODO</i>
-	</div>
-
 	</main>
 </body>

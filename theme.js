88282803feacf052bc39c1bba6f5622eee2a1c56
Duanin2 <duanin2@duanin2.top>
2024-10-02 09:17:12 +0200

    Impove readability and default to browser color scheme
    Improves the readability of the code by using whitespace better, and choosing better variable names. Also defaults the color scheme to the browser's color scheme, instead of defaulting to light mode.

--- theme.js
+++ theme.js
@@ -1,22 +1,21 @@
-var h=document.querySelectorAll("head")[0];
-var s=document.getElementById("switch");
-var dark=false;
-if(document.cookie==""){
-	document.cookie="dark=0;expires=${Date.now()+3600};path=/;SameSite=None";
+let head = document.querySelector("head");
+let themeSwitch = document.getElementById("switch");
+let isDark = false;
+if (document.cookie == "") {
+	document.cookie = "dark=" + window.matchMedia("prefers-color-scheme: light").matches ? "0" : "1" + ";expires=${Date.now()+3600};path=/;SameSite=None";
 }
-if(document.cookie.match("dark=1")!=null) switchTheme();
-function switchTheme(){
-	if(!dark){
-	h.innerHTML+="<link rel=stylesheet href=/dark.css>";
-	dark=true;
-	s.innerHTML="light ";
-	document.cookie="dark=1;expires=${Date.now()+3600};path=/";
-	}
-	else {
-	h.innerHTML+="<link rel=stylesheet href=/light.css>";
-	dark=false;
-	s.innerHTML="dark ";
-	document.cookie="dark=0;expires=${Date.now()+3600};path=/";
+if (document.cookie.match("dark=1") != null) switchTheme();
+function switchTheme() {
+	if (!isDark) {
+		head.innerHTML += "<link rel=stylesheet href=/dark.css>";
+		dark = true;
+		themeSwitch.innerHTML = "light ";
+		document.cookie = "dark=1;expires=${Date.now()+3600};path=/";
+	} else {
+		head.innerHTML += "<link rel=stylesheet href=/light.css>";
+		isDark = false;
+		themeSwitch.innerHTML="dark ";
+		document.cookie="dark=0;expires=${Date.now()+3600};path=/";
 	}
 }
-console.log(h.innerHTML);
+console.log(head.innerHTML);

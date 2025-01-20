c278312c5c66bf86e7afe70f5c2f8cb572a0315a
Duanin2 <duanin2-git@duanin2.top>
2025-01-20 19:29:43 +0100

    Fix broken theming and some theme cookie expiration dates

--- theme.js
+++ theme.js
@@ -1,21 +1,18 @@
 let head = document.querySelector("head");
 let themeSwitch = document.getElementById("switch");
 let isDark = false;
-if (document.cookie == "") {
-	document.cookie = "dark=" + window.matchMedia("prefers-color-scheme: light").matches ? "0" : "1" + ";expires=${Date.now()+3600};path=/;SameSite=None";
-}
+if (document.cookie == "") document.cookie = `dark=${window.matchMedia("prefers-color-scheme: light").matches ? "0" : "1"};expires=${Date.now()+3600};path=/;SameSite=None`;
 if (document.cookie.match("dark=1") != null) switchTheme();
 function switchTheme() {
 	if (!isDark) {
 		head.innerHTML += "<link rel=stylesheet href=/dark.css>";
-		dark = true;
+		isDark = true;
 		themeSwitch.innerHTML = "light ";
-		document.cookie = "dark=1;expires=${Date.now()+3600};path=/";
+		document.cookie = `dark=1;expires=${Date.now()+3600};path=/`;
 	} else {
 		head.innerHTML += "<link rel=stylesheet href=/light.css>";
 		isDark = false;
 		themeSwitch.innerHTML="dark ";
-		document.cookie="dark=0;expires=${Date.now()+3600};path=/";
+		document.cookie=`dark=0;expires=${Date.now()+3600};path=/`;
 	}
 }
-console.log(head.innerHTML);
